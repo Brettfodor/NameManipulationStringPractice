4e8223c3ea11526913d2b95b958f8e1f87978789
Brett Fodor <bmattmusic@gmail.com>
2019-09-04 15:09:43 -0400

    third commit

--- NameManipulationStringPractice/Program.cs
+++ NameManipulationStringPractice/Program.cs
@@ -11,10 +11,21 @@ namespace NameManipulationStringPractice
             string firstName;
             firstName = Console.ReadLine();
 
-            Console.WriteLine("Your name is: " + firstName);
+            Console.WriteLine("Your first name is: " + firstName);
+
+            Console.WriteLine("What is your last name? ");
+
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Your full name is: " + firstName.ToLower()+ " " + lastName.ToLower());
+
+            if(firstName.Length > lastName.Length) Console.WriteLine("Your first name is longer than your last name");
+            
+            if(lastName.Length > firstName.Length) Console.WriteLine("Your last  name is longer than your first name");
+
+            if(lastName.Length == firstName.Length) Console.WriteLine("Your first and last name are equal in character length");
             
 
-            
         }
     }
 }
